a772c59c4c30d15fe2917eff6c07d758fd7fab18
Roy <toledoroy@gmail.com>
2022-02-14 11:19:05 -0800

    Basic MUI Custom Theme & Added Some Classes

--- src/App.js
+++ src/App.js
@@ -13,19 +13,36 @@ import Me from "./pages/Me";
 
 import Header from "./components/Header";
 
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { grey } from '@mui/material/colors';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: grey[900],
+    },
+    secondary: {
+      main: grey[50],
+    }
+  }
+});
+
 export default function App() {
   return (
     <Router>
-      <div>
-        <Header />
-        <Box sx={{ m: 4 }} />
-        <Routes>
-          <Route path="/artist/new" element={<NewArtist />} />
-          <Route path="/artist/:id" element={<Artist />} />
-          <Route path="/me" element={<Me />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
+      <div className="app">
+        <ThemeProvider theme={theme}>
+            <Header />
+            <Box sx={{ m: 4 }} />
+            <Routes>
+              <Route path="/artist/new" element={<NewArtist />} />
+              <Route path="/artist/:id" element={<Artist />} />
+              <Route path="/me" element={<Me />} />
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </ThemeProvider>
       </div>
     </Router>
+    
   );
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,13 +3,13 @@ import { Box, Link } from "@mui/material";
 
 export default function Header() {
   return (
-    <Box sx={{
+    <Box className="app-header" sx={{
       display: 'grid',
       justifyContent: "space-around",
       borderBottom: "3px solid black",
       padding: "2em",
     }}>
-      <Link href="/" underline="none" variant="h3">SUPERTRUE</Link>
+      <Link href="/" underline="none" variant="h3" className="app-logo">SUPERTRUE</Link>
     </Box>
   );
 }
