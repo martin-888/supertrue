f19be1a17f5a4d19252748c9e9c8aff0f3f60b86
Martin Babinec <M@rtinBabinec.cz>
2022-02-14 11:10:12 -0300

    Added me page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import {
 import Home from "./pages/Home";
 import Artist from "./pages/Artist";
 import NewArtist from "./pages/NewArtist";
+import Me from "./pages/Me";
 
 import Header from "./components/Header";
 
@@ -21,6 +22,7 @@ export default function App() {
         <Routes>
           <Route path="/artist/new" element={<NewArtist />} />
           <Route path="/artist/:id" element={<Artist />} />
+          <Route path="/me" element={<Me />} />
           <Route path="/" element={<Home />} />
         </Routes>
       </div>
