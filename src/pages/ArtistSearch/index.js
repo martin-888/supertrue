d95882cc0e5baf2bc723522bec982e3120ff50dc
Roy <toledoroy@gmail.com>
2022-02-15 12:36:07 -0800

    Search Page as Gallery Page

--- src/pages/ArtistSearch/index.js
+++ src/pages/ArtistSearch/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { TextField, Typography, Button, Grid, Box, List, ListItem, Container, CircularProgress } from "@mui/material";
+import { Link, TextField, Typography, Button, Grid, Box, List, ListItem, Container, CircularProgress } from "@mui/material";
 import { getArtists } from "../../api";
 // import ArtistBlock from "components/ArtistBlock";
 import ArtistNFT from "components/ArtistNFT";
@@ -9,7 +9,7 @@ import __ from "helpers/__";
 /**
  * Component: NFT Search Page
  */
-export default function ArtistSearch() {
+export default function ArtistSearch({view}) {
   const [searchQuery, setSearchQuery] = useState('');
   const [loading, setLoading] = useState(true);
   const [artists, setArtists] = useState([]);
@@ -80,9 +80,16 @@ export default function ArtistSearch() {
 
     return (
       <>
-        <Typography maxWidth="md" variant="h5" className="explanation" sx={{my:6}}>
+        {view!=='gallery' && 
+        <Typography maxWidth="md" variant="h5" className="explanation" sx={{mt:6, mb:10}}>
           Mint your favorite new artist to receive a dated NFT with your disovery date and your supertrue follower count. Display your music finds in your gallery. Funds are held for the artist minus transaction fees. 
         </Typography>
+        }
+        
+        {view!=='gallery' && <Box key='h2' sx={{mb:3}}>
+            <Typography variant="h2" >NEWLY MINTED</Typography>
+          </Box>}
+
         <Grid container spacing={8}>
           {filteredArtists.map((artist, index) => (
             <Grid item key={artist.id} className="artist" xs={8} md={4}>
@@ -97,11 +104,21 @@ export default function ArtistSearch() {
 
   return (
     <>
-      <Container maxWidth="sm">
-        <TextField autoFocus fullWidth id="standard-basic" label="Search Artist" variant="filled" onChange={e => setSearchQuery(e.target.value)} />
-      </Container>
       
+      {view!=='gallery' && 
+      <Container maxWidth="md">
+        <TextField autoFocus sx={{minWidth:'400px'}} id="standard-basic" label="Search Artist" variant="filled" onChange={e => setSearchQuery(e.target.value)} />
+      </Container>
+      }
       <Container maxWidth="md" sx={{ my: 8 }}>
+        {view==='gallery' && 
+        <Box sx={{mb:6}}>
+          <Typography variant="h1" >NFT GALLERY</Typography>
+          <Typography variant="h6" className="explanation" sx={{my:2}}>
+            All your shiny finds are displayed here. <Link variant="text" href={`/artist/new`}>Mint a new artist</Link>
+          </Typography>
+        </Box>
+        }
         {getContent()}
       </Container>
     </>
