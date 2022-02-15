4e65110d0ebe7f1153265c40251b78672ca5ece8
Roy <toledoroy@gmail.com>
2022-02-15 11:06:13 -0800

    Updated Helper Func.

--- src/helpers/__.js
+++ src/helpers/__.js
@@ -2,7 +2,8 @@
  * General Helper Functions
  */
 export const __ = {
-    getArtistNFTImage(artist){ return `https://us-central1-supertrue-5bc93.cloudfunctions.net/api/artist/${artist.id}/image/${artist.minted}`; }
+    getArtistNFTImage(artist){ return `https://us-central1-supertrue-5bc93.cloudfunctions.net/api/artist/${artist.id}/image/${artist.minted}`; },
+    getArtistImage(artist){ return `https://storage.googleapis.com/supertrue-5bc93.appspot.com/${artist.id}.jpg`; },
 };
   
 export default __;
