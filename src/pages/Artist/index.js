099e67fc8b97f0d53bde37a578b8f905c3aeb4bb
Roy <toledoroy@gmail.com>
2022-02-22 14:55:17 -0800

    Updated the mint page

--- src/pages/Artist/index.js
+++ src/pages/Artist/index.js
@@ -1,13 +1,21 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Contract } from "@ethersproject/contracts";
-import { Box, Button, CircularProgress, Grid, Typography } from "@mui/material";
-
+import { Container, Box, Button, CircularProgress, Grid, Typography } from "@mui/material";
 import { abis } from "../../contracts";
 import { getArtist } from "../../api";
 import useWeb3Modal from "../../hooks/useWeb3Modal";
 import __ from "helpers/__";
+// import millify from "millify";
+// import moment from 'moment';
+import './Artist.scss';
 
+// import ArtistBlock from "components/ArtistBlock";
+
+
+/**
+ * Component: Single Artist Page
+ */
 export default function Artist() {
   const [provider, loadWeb3Modal, logoutOfWeb3Modal] = useWeb3Modal();
   const [loading, setLoading] = useState(true);
@@ -109,49 +117,62 @@ export default function Artist() {
         <Typography variant="h5">Artist Not Found</Typography>
       </Grid>
     );
-  }
+  };
 
   return (
-    <Grid
-      container
-      direction="column"
-      justifyContent="center"
-      alignItems="center"
-    >
-      <img width="400" height="400" src={__.getArtistNFTImage(artist, `${(artist.minted >= totalSupply) ? (artist.minted+1) : (totalSupply+1)}`)} />
-      <Box sx={{ mt: 6, mb:2 }}>
-        <Typography variant="h5">Price: {price / 10**18} ETH</Typography>
-      </Box>
+    <Container maxWidth="md">
+    <Grid container className="artist-single">
 
-      {minting && (
-        <Box sx={{ my: 4 }}>
-          <CircularProgress />
-        </Box>
-      )}
+      <Grid item className="image" md={6}>
+        <img src={__.getArtistNFTImage(artist, `${(artist.minted >= totalSupply) ? (artist.minted+1) : (totalSupply+1)}`)} />
+      </Grid>
+      <Grid item className="details" md={6}>
 
-      {minted && (
-        <Box sx={{ my: 4 }}>
-          <Button
-            variant="contained"
-            color="success"
-            target="_blank"
-            href={`https://testnets.opensea.io/assets/${artist.contractAddress}/${totalSupply + 1}`}
-          >
-            Show On OpenSea
-          </Button>
+        <Typography variant="h3" component="h3" className="name">{artist.name}</Typography>
+        {/* <Typography variant="subtitle1">Followers: {millify(artist.followers)}</Typography> */}
+        {/* <Typography variant="subtitle1">Date of Discovery: {moment(artist.created).format('MM.DD.YYYY')}</Typography> */}
+        {/* <Typography variant="subtitle1">Supertrue #{artist.minted}</Typography> */}
+        
+        <Box sx={{ my: 3 }}>
+          <Typography variant="h5" className="price">
+            <label>Price:</label> {price / 10**18} ETH</Typography>
         </Box>
-      )}
-      
-      <Box sx={{ my: 4 }}>
-        <Button
-          size="large"
-          variant="contained"
-          onClick={!provider ? loadWeb3Modal : mintNFT}
-          disabled={minting}
-        >
-          Mint Fan #{totalSupply + 1}
-        </Button>
-      </Box>
+
+        <Typography variant="subtitle1">Price goes up per each additional NFT created, Funds for unclaimed profiles are held for the artist minus service fees.</Typography>
+
+        {minted && (
+            <Box sx={{ my: 2 }}>
+              <Button
+                variant="contained"
+                color="success"
+                target="_blank"
+                href={`https://testnets.opensea.io/assets/${artist.contractAddress}/${totalSupply + 1}`}
+              >
+                Show On OpenSea
+              </Button>
+            </Box>
+          )}
+
+        <Box className="actions">
+          <Box>
+            {minting ? (
+              <CircularProgress />
+            )
+            : (
+              <Button
+                size="large"
+                variant="contained"
+                onClick={!provider ? loadWeb3Modal : mintNFT}
+                disabled={minting}
+              >
+                Mint Fan #{totalSupply + 1}
+              </Button>
+          )}
+         </Box>
+          
+        </Box>
+      </Grid>
     </Grid>
+    </Container> 
   );
 }
