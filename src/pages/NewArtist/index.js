351bc726b94d077d5b38d6bb71cc2ce88794fb2c
Roy <toledoroy@gmail.com>
2022-02-22 14:57:36 -0800

    Added some Error handling

--- src/pages/NewArtist/index.js
+++ src/pages/NewArtist/index.js
@@ -38,19 +38,20 @@ export default function NewArtist() {
   const [existingArtistID, setExistingArtistID] = useState(null);
 
   useEffect(() => {
-    api.getArtists().then(resp => setArtists(resp.artists))
+    //Fetch Artists
+    api.getArtists().then(resp => setArtists(resp.artists));
   }, []);
 
   useEffect(() => {
+    //Extract Instagram handle
     const match = instagram.match(/instagram.com\/([\w-]+)/);
     setInstagramValid(!!match);
 
+    //Check if Artist Already Exists
     const artist = artists?.find(artist => artist.igHandle === match?.[1])
-
     setExistingArtistID(artist?.id);
 
     const exists = !!match?.[1] && !!artists.find(artist => artist.igHandle === match?.[1]);
-
     setArtistExists(exists);
 
     if (exists) {
@@ -70,14 +71,24 @@ export default function NewArtist() {
   const createArtist = async () => {
     setLoading(true);
 
-    const { receipt, tx } = await create({ provider, instagram: instagramHandle, name })
-    // .catch() // TODO handle errors
-
-    console.log({ receipt, tx });
-
-    const { artist } = await api.createArtist({ tx: receipt.transactionHash });
-
-    setTimeout(() => navigate(`/artist/${artist.id}`), 5000);
+    try{
+      //Create Artist on Chain
+      const { receipt, tx } = await create({ provider, instagram: instagramHandle, name });
+      //Log
+      console.log({ receipt, tx });
+      //Create Artist in DB
+      const { artist } = await api.createArtist({ tx: receipt.transactionHash });
+      //Redirect to Artist Page
+      setTimeout(() => {
+        if(artist && artist?.id) navigate(`/artist/${artist.id}`);
+        else console.error("New Artist not Found", {artist, tx, receipt});
+      }, 2000);
+    }
+    catch(error) {
+      console.error("Failed to create Artist", error);
+      // TODO handle errors / show the user an error message
+    }
+    setLoading(false);
   };
 
   return (
@@ -89,65 +100,67 @@ export default function NewArtist() {
           justifyContent="center"
           alignItems="center"
         >
-          <Typography
-            variant="h3"
-            component="h3"
-            sx={{ opacity: artistExists ? "0.3" : "1" }}
-          >
+          <Typography variant="h3" component="h3" sx={{ opacity: artistExists ? "0.3" : "1" }}>
             Add {name || "?"}
           </Typography>
-          <Box sx={{ m: 2 }} />
-          <TextField
-            autoFocus
-            fullWidth
-            error={!nameValid}
-            color={nameValid ? "success" : null}
-            id="standard-basic"
-            label="Name"
-            variant="standard"
-            value={name}
-            onChange={e => setName(e.target.value)}
-          />
-          <Box sx={{ m: 2 }} />
-          <TextField
-            fullWidth
-            error={instagramTouched && !instagramValid}
-            color={!(instagramTouched && !instagramValid) ? "success" : null}
-            placeholder="https://www.instagram.com/drake/"
-            id="standard-basic"
-            label="Instagram Link"
-            variant="standard"
-            onChange={e => {
-              setInstagram(e.target.value);
-              setInstagramTouched(true);
-            }}
-          />
-          <Box sx={{ m: 4 }} />
+          
+          <Box sx={{ m: 2 }}>
+            <TextField
+              autoFocus
+              fullWidth
+              error={!nameValid}
+              color={nameValid ? "success" : null}
+              id="standard-basic"
+              label="Name"
+              variant="standard"
+              value={name}
+              onChange={e => setName(e.target.value)}
+            />
+          </Box>
+
+          <Box sx={{ m: 2 }}>
+            <TextField
+              fullWidth
+              error={instagramTouched && !instagramValid}
+              color={!(instagramTouched && !instagramValid) ? "success" : null}
+              placeholder="https://www.instagram.com/drake/"
+              id="standard-basic"
+              label="Instagram Link"
+              variant="standard"
+              onChange={e => {
+                setInstagram(e.target.value);
+                setInstagramTouched(true);
+              }}
+            />
+          </Box>
+
           {loading && (
-            <>
+            <Box sx={{ m: 4 }}>
               <CircularProgress />
-              <Box sx={{ m: 4 }} />
-            </>
+            </Box>
           )}
-          {artistExists ? (
-            <Button
-              size="large"
-              variant="contained"
-              href={`/artist/${existingArtistID}`}
-            >
-              Open {artists.find(artist => artist.id === existingArtistID).name}
-            </Button>
-            ) : (
-            <Button
-              disabled={!instagramValid || !nameValid || loading}
-              size="large"
-              variant="contained"
-              onClick={!provider ? loadWeb3Modal : createArtist}
-            >
-              Create {name}
-            </Button>
-          )}
-          <Box sx={{ m: 4 }} />
+
+          <Box sx={{ m: 4 }}>
+            {artistExists ? (
+              <Button
+                size="large"
+                variant="contained"
+                href={`/artist/${existingArtistID}`}
+              >
+                Open {artists.find(artist => artist.id === existingArtistID).name}
+              </Button>
+              ) : (
+              <Button
+                disabled={!instagramValid || !nameValid || loading}
+                size="large"
+                variant="contained"
+                onClick={!provider ? loadWeb3Modal : createArtist}
+              >
+                Create {name}
+              </Button>
+            )}
+          </Box>
+          
         </Grid>
       </Container>
     </>
