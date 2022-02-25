4624d39be53d791527ef7e6297166ff21949a23f
Roy <toledoroy@gmail.com>
2022-02-24 16:59:42 -0800

    Changed Heading Fonts

--- src/App.js
+++ src/App.js
@@ -32,32 +32,32 @@ const theme = createTheme({
     fontFamily: ['Space Mono', '"Montserrat"', 'Open Sans'].join(','),
 
     h1:{
-      fontFamily: 'DM Serif Display, Open Sans',
+      fontFamily: "DM Serif Display, serif",
       fontWeight: 'bold',
       fontSize: '2rem',
     },
     h2:{
-      fontFamily: 'DM Serif Display, Open Sans',
+      fontFamily: "DM Serif Display, serif",
       fontWeight: 'bold',
       fontSize: '1.8rem',
     },
     h3:{
-      fontFamily: 'DM Serif Display, Open Sans',
+      fontFamily: "DM Serif Display, serif",
       fontWeight: 'bold',
       fontSize: '1.6rem',
     },
     h4:{
-      fontFamily: 'DM Serif Display, Open Sans',
+      fontFamily: "DM Serif Display, serif",
       fontWeight: 'bold',
       fontSize: '1.4rem',
     },
     h5:{
-      fontFamily: 'DM Serif Display, Open Sans',
+      fontFamily: "DM Serif Display, serif",
       fontWeight: 'bold',
       fontSize: '1.2rem',
     },
     h6:{
-      fontFamily: 'DM Serif Display, Open Sans',
+      fontFamily: "DM Serif Display, serif",
       fontWeight: 'bold',
       fontSize: '1.1rem',
     },
