23ccaebee632167bca04857a3939c434821aa546
Martin Babinec <M@rtinBabinec.cz>
2022-02-27 23:43:14 -0300

    New design for new NFTs

--- src/helpers/__.js
+++ src/helpers/__.js
@@ -5,8 +5,8 @@
 const url = "https://us-central1-supertrue-5bc93.cloudfunctions.net";
 
 export const __ = {
-    getNFTImage(artistId, tokenId){ return `${url}/api/artist/${artistId}/image/${tokenId}`; },
-    getArtistNFTImage(artist, fanNumber){ return `${url}/api/artist/${artist.id}/image/${fanNumber ? fanNumber : artist.minted+1}`; },
+    getNFTImage(artistId, tokenId){ return `${url}/api/artist/${artistId}/image${artistId > 7 ? "-new" : ""}/${tokenId}`; },
+    getArtistNFTImage(artist, fanNumber){ return `${url}/api/artist/${artist.id}/image${artist.id > 7 ? "-new" : ""}/${fanNumber ? fanNumber : artist.minted+1}`; },
     getArtistImage(artist){ return `https://storage.googleapis.com/supertrue-5bc93.appspot.com/${artist.id}.jpg`; },
 };
 
