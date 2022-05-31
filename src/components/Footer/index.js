bfa0f8e5577ba267e8774651b6afb9bc4497eab5
Joris Zierold <contact@joriszierold.com>
2022-05-31 23:21:32 +0100

    add background image to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,15 +1,44 @@
 import React from "react";
-import { Box, Link, Container } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
 // import './Footer.scss';
+import footerImage from "../../assets/img/myCanvas-inverted_1000x350px.png";
+
+const styles = {
+  bgImageContainer: { height: "350px", marginTop: "60px" },
+  bgImageOverlay: {
+    height: "350px",
+    width: "100%",
+    backgroundImage: "linear-gradient(#ffffff, #ffffffc9, #ffffff)",
+    zIndex: 2,
+    position: "absolute",
+  },
+  bgImage: {
+    height: "350px",
+    width: "100%",
+    maxHeight: "700px",
+    backgroundImage: `url(${footerImage})`,
+    opacity: 0.5,
+    backgroundRepeat: "repeat-x",
+    backgroundPosition: "center",
+    zIndex: 1,
+    position: "absolute",
+  },
+};
 
 export default function Footer(props) {
   return (
-    <Container maxWidth="md">
-    <Box className="app-footer">
-      <div className="inner">
-        {props.children}
-      </div>
-    </Box>
-    </Container>
+    <>
+      <Box fullWidth sx={styles.bgImageContainer}>
+        <Box sx={styles.bgImageOverlay} />
+        <Box sx={styles.bgImage} />
+      </Box>
+      <Container maxWidth="md">
+        <Box className="app-footer">
+          <Typography className="inner" textAlign="center" pb={2}>
+            {props.children}
+          </Typography>
+        </Box>
+      </Container>
+    </>
   );
 }
