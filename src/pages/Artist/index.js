545102249fa70ed5cd7b7f0002e37498a678bbec
Joris Zierold <contact@joriszierold.com>
2022-06-02 19:23:00 +0100

    add about section to profile and modify faq to accordion

--- src/pages/Artist/index.js
+++ src/pages/Artist/index.js
@@ -2,32 +2,49 @@ import React, { useEffect, useState } from "react";
 import { gql, useQuery } from "@apollo/client";
 import { useParams } from "react-router-dom";
 import { Contract, utils } from "ethers";
-import { Container, Box, Button, CircularProgress, Grid, Typography, Link } from "@mui/material";
+import {
+  Container,
+  Box,
+  Button,
+  CircularProgress,
+  Grid,
+  Typography,
+  Link,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 import { abis } from "../../contracts";
 import useWeb3Modal from "../../hooks/useWeb3Modal";
 import __ from "helpers/__";
 import useAccountBalance from "../../hooks/useAccountBalance";
 
-import './Artist.scss';
+import "./Artist.scss";
 
 const ARTIST_QUERY = gql`
-    query getArtist($artistId: Int) {
-      collections(where: {artistId: $artistId}) {
-        id
-        artistId
-        minted
-        name
-        symbol
-        instagram
-        address
-        price
-      }
+  query getArtist($artistId: Int) {
+    collections(where: { artistId: $artistId }) {
+      id
+      artistId
+      minted
+      name
+      description
+      symbol
+      instagram
+      address
+      price
     }
+  }
 `;
 
 async function mint({ provider, contractAddress, price }) {
-  const supertrueNFT = new Contract(contractAddress, abis.supertrueNFT, provider.getSigner());
+  const supertrueNFT = new Contract(
+    contractAddress,
+    abis.supertrueNFT,
+    provider.getSigner()
+  );
 
   const address = provider.getSigner().getAddress();
 
@@ -46,7 +63,7 @@ export default function Artist() {
   const balance = useAccountBalance();
   const { id } = useParams();
   const { data, loading, error } = useQuery(ARTIST_QUERY, {
-    variables: { artistId: Number(id) }
+    variables: { artistId: Number(id) },
   });
 
   const [minting, setMinting] = useState(false);
@@ -55,8 +72,11 @@ export default function Artist() {
   const waitTime = 2000;
 
   useEffect(() => {
-    if(error) {
-      console.error("Failed to fetch Artist:" + id + " data: ", {data, error});
+    if (error) {
+      console.error("Failed to fetch Artist:" + id + " data: ", {
+        data,
+        error,
+      });
     }
     setArtist(data?.collections?.[0]);
   }, [data]);
@@ -68,9 +88,13 @@ export default function Artist() {
     }
 
     setMinting(true);
-    await mint({ provider, contractAddress: artist.address, price: artist.price })
+    await mint({
+      provider,
+      contractAddress: artist.address,
+      price: artist.price,
+    })
       .then(() => setTimeout(() => setMinted(true), waitTime))
-      .catch(err => console.error(err.message))
+      .catch((err) => console.error(err.message))
       .finally(() => setTimeout(() => setMinting(false), waitTime));
   };
 
@@ -90,96 +114,155 @@ export default function Artist() {
   if (!artist) {
     return (
       <Container maxWidth="md">
-      <Grid>
-        <Typography variant="h5">Requested Artist Not Found</Typography>
-      </Grid>
+        <Grid>
+          <Typography variant="h5">Requested Artist Not Found</Typography>
+        </Grid>
       </Container>
     );
   }
 
   return (
     <Container maxWidth="md">
-    <Grid container className="artist-single">
+      <Grid container className="artist-single">
+        <Grid item className="image" md={6}>
+          <img
+            src={__.getNFTImage(artist.artistId, artist.minted + 1)}
+            alt={artist.name}
+          />
+        </Grid>
 
-      <Grid item className="image" md={6}>
-        <img src={__.getNFTImage(artist.artistId, artist.minted+1)} alt={artist.name} />
-      </Grid>
+        <Grid item className="details" md={6}>
+          <Typography variant="h2" className="title">
+            Mint {artist.name}
+          </Typography>
 
-      <Grid item className="details" md={6}>
-        <Typography variant="h2" className="title">Mint {artist.name}</Typography>
-
-        <Typography variant="subtitle1" className="title">
-          <Link target="_blank" href={`https://www.instagram.com/${artist.instagram}`}>
-            @{artist.instagram}
-          </Link>
-        </Typography>
-
-        <Box sx={{ my: 3 }}>
-          <Typography variant="h5" className="price">
-            <label>Price:</label> </Typography> <Typography>{artist.price / 10**18} ETH </Typography>
-			<br />
-        <Typography variant="subtitle2">Price goes up per each additional NFT created.</Typography>
-
-        </Box>
-
-        {minted && (
-          <Box sx={{ my: 2 }}>
-            <Button
-              variant="contained"
-              color="success"
+          <Typography variant="subtitle1" className="title">
+            <Link
               target="_blank"
-              href={`https://testnets.opensea.io/assets/${artist.address}/${artist.minted + 1}`}
+              href={`https://www.instagram.com/${artist.instagram}`}
             >
-              Show On OpenSea
-            </Button>
+              @{artist.instagram}
+            </Link>
+          </Typography>
+
+          <Box sx={{ my: 3 }}>
+            <Typography variant="h5" className="price">
+              <label>Price:</label>{" "}
+            </Typography>{" "}
+            <Typography>{artist.price / 10 ** 18} ETH </Typography>
+            <br />
+            <Typography variant="subtitle2">
+              Price goes up per each additional NFT created.
+            </Typography>
           </Box>
-        )}
 
-        <Box className="actions">
-          <Box>
-            {minting ? (
-              <CircularProgress />
-            )
-            : (
+          {minted && (
+            <Box sx={{ my: 2 }}>
               <Button
-                size="large"
                 variant="contained"
-                onClick={!provider ? loadWeb3Modal : mintNFT}
-                disabled={minting}
+                color="success"
+                target="_blank"
+                href={`https://testnets.opensea.io/assets/${artist.address}/${
+                  artist.minted + 1
+                }`}
               >
-                Mint Fan #{artist.minted + 1}
+                Show On OpenSea
               </Button>
+            </Box>
           )}
-         </Box>
 
+          <Box className="actions">
+            <Box>
+              {minting ? (
+                <CircularProgress />
+              ) : (
+                <Button
+                  size="large"
+                  variant="contained"
+                  onClick={!provider ? loadWeb3Modal : mintNFT}
+                  disabled={minting}
+                >
+                  Mint Fan #{artist.minted + 1}
+                </Button>
+              )}
+            </Box>
+          </Box>
+        </Grid>
+        <Box className="about">
+          <Typography variant="h4">About</Typography>
+          <Typography>{artist.description}</Typography>
         </Box>
 
+        <Typography mb={"10px"} variant="h4">
+          FAQ
+        </Typography>
+        <Accordion className="faqs">
+          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+            <Typography variant="h4">WHY MINT A SUPERTRUE NFT?</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              Supertrue mints a discovery NFT showing what date you started
+              officially supporting the artist. In lieu of owning an album, we
+              give you a personal track record of artists you truly believe in.
+              It’s like creating a digital archive of artists you’ve gotten
+              behind. This becomes more interesting and useful as you build your
+              collection.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion className="faqs">
+          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+            <Typography variant="h4">WHAT’S IN IT FOR THE ARTIST</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              We built this with the artists in mind. Funds are held for the
+              artist to claim minus our service fee. When an artist sets out to
+              build an instagram following they get nothing. When an artist sets
+              out to build a supertrue following and reaches 1,000 fans, they
+              have 8k USD to master their album. At 10,000 fans they have
+              100,000 USD to go on tour. All the while you benefit by getting
+              credit you deserve of supporting them when they needed it the
+              most.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion className="faqs">
+          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+            <Typography variant="h4">
+              WHAT HAPPENS WHEN AN ARTIST I BELIEVE IN GROWS
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              Congratulations! You’ve helped someone on their way up and now
+              they’re on they’re on their way up. Supertrue saves your spot in
+              time that you’ve backed that artist, and gives them the ability to
+              reward their supertrue fans. How exactly they do it is up to them.
+              We suggest to artist to give early access, exclusive shows, and
+              special merch only available to their supertrue fans.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion className="faqs">
+          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+            <Typography variant="h4">
+              CAN I SELL OR TRADE MY SUPERTRUE NFT
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              If you’re ready auction off one of the special pieces of your
+              collection as it grows in value, that’s up to you. The artist will
+              get a 20% cut of your sale and everyone will be happy. We
+              understand the feeling of collecting your winnings and using them
+              to find someone new who is about to make their start. Happy
+              collecting! Happy trading!
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
       </Grid>
-
-      <Box sx={{ my: 3 }} className="additional-details">
-        <Typography variant="h4">WHY MINT A SUPERTRUE NFT?</Typography>
-        <Typography>
-          Supertrue mints a discovery NFT showing what date you started officially supporting the artist. In lieu of owning an album, we give you a personal track record of artists you truly believe in. It’s like creating a digital archive of artists you’ve gotten behind. This becomes more interesting and useful as you build your collection.
-        </Typography>
-
-        <Typography variant="h4">WHAT’S IN IT FOR THE ARTIST</Typography>
-        <Typography>
-          We built this with the artists in mind. Funds are held for the artist to claim minus our service fee. When an artist sets out to build an instagram following they get nothing. When an artist sets out to build a supertrue following and reaches 1,000 fans, they have 8k USD to master their album. At 10,000 fans they have 100,000 USD to go on tour. All the while you benefit by getting credit you deserve of supporting them when they needed it the most.
-        </Typography>
-
-        <Typography variant="h4">WHAT HAPPENS WHEN AN ARTIST I BELIEVE IN GROWS</Typography>
-        <Typography>
-          Congratulations! You’ve helped someone on their way up and now they’re on they’re on their way up. Supertrue saves your spot in time that you’ve backed that artist, and gives them the ability to reward their supertrue fans. How exactly they do it is up to them. We suggest to artist to give early access, exclusive shows, and special merch only available to their supertrue fans.
-        </Typography>
-
-        <Typography variant="h4">CAN I SELL OR TRADE MY SUPERTRUE NFT</Typography>
-        <Typography>
-          If you’re ready auction off one of the special pieces of your collection as it grows in value, that’s up to you. The artist will get a 20% cut of your sale and everyone will be happy. We understand the feeling of collecting your winnings and using them to find someone new who is about to make their start. Happy collecting! Happy trading!
-        </Typography>
-
-      </Box>
-
-    </Grid>
     </Container>
   );
 }
