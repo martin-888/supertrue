8d43cb9b7ae267d08db3e2517ad06e5aa7ffc4a7
Martin Babinec <M@rtinBabinec.cz>
2022-06-15 20:36:19 -0400

    Changed image urls

--- src/helpers/__.js
+++ src/helpers/__.js
@@ -2,12 +2,12 @@
  * General Helper Functions
  */
 
-const url = "https://us-central1-supertrue-5bc93.cloudfunctions.net";
+const url = "https://storage.googleapis.com/supertrue-5bc93.appspot.com";
 
 export const __ = {
-    getNFTImage(artistId, tokenId){ return `${url}/api/artist/${artistId}/image/${tokenId}`; },
-    getArtistNFTImage(artist, fanNumber){ return `${url}/api/artist/${artist.id}/image/${fanNumber ? fanNumber : artist.minted+1}`; },
-    getArtistImage(artistId){ return `https://storage.googleapis.com/supertrue-5bc93.appspot.com/profile/${artistId}.jpg`; },
+    getNFTImage(artistId, tokenId){ return `${url}/nfts/${artistId}/${tokenId}.png`; },
+    getArtistNFTImage(artist, fanNumber){ return `${url}/nfts/${artist.id}/${fanNumber ? fanNumber : artist.minted+1}.png`; },
+    getArtistImage(artistId){ return `${url}/profile/${artistId}.jpg`; },
 };
 
 export default __;
