b2b80532c20f9c89adefa3eb4431c2516882d1b1
Martin Babinec <M@rtinBabinec.cz>
2022-06-17 16:59:54 -0400

    Search refactor

--- src/pages/ArtistSearch/index.js
+++ src/pages/ArtistSearch/index.js
@@ -3,7 +3,6 @@ import { gql, useQuery } from "@apollo/client";
 import {
   TextField,
   Typography,
-  Button,
   Grid,
   Box,
   CircularProgress,
@@ -19,9 +18,6 @@ const COLLECTIONS_QUERY = gql`
       minted
       name
       instagram
-      owner {
-        id
-      }
     }
   }
 `;
@@ -29,7 +25,7 @@ const COLLECTIONS_QUERY = gql`
 /**
  * Component: NFT Search Page
  */
-export default function ArtistSearch({ view }) {
+export default function ArtistSearch() {
   const [searchQuery, setSearchQuery] = useState("");
   const [filteredArtists, setFilteredArtists] = useState([]);
   const { data, loading, error } = useQuery(COLLECTIONS_QUERY);
@@ -77,60 +73,58 @@ export default function ArtistSearch({ view }) {
           justifyContent="center"
           alignItems="center"
         >
-          <Typography variant="h5">No Artists Available</Typography>
+          <Typography variant="h5">No collections found</Typography>
         </Grid>
       );
     }
     if (!filteredArtists.length) {
       return (
-        <Grid
-          container
-          direction="column"
-          justifyContent="center"
-          alignItems="center"
-        >
+        <>
           <Typography variant="h5" component="h3" align="center">
-            Sorry we couldn’t find "{searchQuery}".
-            <br />
-            <br />
-            You can be the first!
+            No collection found for "{searchQuery}"
           </Typography>
-          <Box sx={{ m: 2 }} />
-          <Button
-            variant="contained"
-            size="large"
-            href={`/artist/new?name=${searchQuery}`}
-          >
-            Add {searchQuery}
-          </Button>
-        </Grid>
+          {/*<Typography variant="h5" component="h3" align="center">*/}
+          {/*  Collection "{searchQuery}" not found.*/}
+          {/*  <br />*/}
+          {/*  <br />*/}
+          {/*  You can be the first!*/}
+          {/*</Typography>*/}
+          {/*<Box sx={{ m: 2 }} />*/}
+          {/*<Button*/}
+          {/*  variant="contained"*/}
+          {/*  size="large"*/}
+          {/*  href={`/new?name=${searchQuery}`}*/}
+          {/*>*/}
+          {/*  Add {searchQuery}*/}
+          {/*</Button>*/}
+        </>
       );
     }
 
     return (
       <>
-        {view !== "gallery" && (
-          <Typography
-            variant="subtitle1"
-            className="explanation"
-            sx={{ mt: 6, mb: 10 }}
-          >
-            Mint your favorite new artist to receive a dated NFT with your
-            disovery date and your supertrue follower count. Display your music
-            finds in your gallery. Funds are held for the artist minus
-            transaction fees.
-          </Typography>
-        )}
-        {view !== "gallery" && (
-          <Box key="h2" sx={{ mb: 3 }}>
-            <Typography variant="h2">FEATURED ARTISTS</Typography>
-          </Box>
+        {searchQuery.length === 0 && (
+          <>
+            <Typography
+              variant="subtitle1"
+              className="explanation"
+              sx={{ mt: 6, mb: 10 }}
+            >
+              Mint your favorite new artist to receive a dated NFT with your
+              disovery date and your supertrue follower count. Display your music
+              finds in your gallery. Funds are held for the artist minus
+              transaction fees.
+            </Typography>
+
+            <Box key="h2" sx={{ mb: 3 }}>
+              <Typography variant="h2">FEATURED ARTISTS</Typography>
+            </Box>
+          </>
         )}
 
         <Grid container spacing={8}>
           {filteredArtists.map((artist, index) => (
             <Grid item key={artist.id} className="artist" xs={12} sm={6} md={4}>
-              {/* <ArtistBlock artist={artist} /> */}
               <ArtistNFT artist={{ ...artist, id: artist.artistId }} />
             </Grid>
           ))}
@@ -141,28 +135,15 @@ export default function ArtistSearch({ view }) {
 
   return (
     <>
-      {view !== "gallery" && (
-        <Grid item sm={6} sx={{ mb: 4 }}>
-          <TextField
-            sx={{ width: "100%" }}
-            id="standard-basic"
-            label="Search Artist"
-            variant="filled"
-            onChange={(e) => setSearchQuery(e.target.value)}
-          />
-        </Grid>
-      )}
-      {/*{view === "gallery" && (*/}
-      {/*  <Box sx={{ mb: 4 }}>*/}
-      {/*    <Typography variant="h1">NFT GALLERY</Typography>*/}
-      {/*    <Typography variant="h6" className="explanation" sx={{ my: 2 }}>*/}
-      {/*      All your shiny finds are displayed here.{" "}*/}
-      {/*      <Link variant="text" href={`/artist/new`}>*/}
-      {/*        Add a new artist*/}
-      {/*      </Link>*/}
-      {/*    </Typography>*/}
-      {/*  </Box>*/}
-      {/*)}*/}
+      <Grid item sm={6} sx={{ mb: 4 }}>
+        <TextField
+          sx={{ width: "100%" }}
+          id="standard-basic"
+          label="Search Artist"
+          variant="filled"
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+      </Grid>
       {getContent()}
     </>
   );
