525189195345ed881efbfce76254ea79f62365ac
dave 3.0 <mathesond2@gmail.com>
2022-07-27 08:25:01 -0400

    log out user upon wallet switch (#28)

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { gql, useQuery } from "@apollo/client";
 import AppBar from "@mui/material/AppBar";
 import { Link } from "react-router-dom";
@@ -16,7 +16,7 @@ import { AccountCircle } from "@mui/icons-material";
 import { useTheme } from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import { useLocation } from "react-router-dom";
-
+import { useAccount } from "wagmi";
 import useLogInWallet from "../../hooks/useLogInWallet";
 import { useAppContext } from "contexts/app";
 import logo from "./logo.png";
@@ -74,6 +74,11 @@ const Header = () => {
   const { data } = useQuery(ME_QUERY);
   const me = data?.me;
   const isLoggedIn = me?.address && address === me.address;
+  const { address: connectedAddress } = useAccount();
+
+  useEffect(() => {
+    if (isLoggedIn && address !== connectedAddress?.toLowerCase()) logout();
+  }, [isLoggedIn, address, connectedAddress, logout]);
 
   const renderMenu = () => {
     if (!isLoggedIn) {
